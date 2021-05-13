093021ca41183c34d65ed5e258ba9116cf8c3eb8
Isaac Rodriguez <isaac.rodriguez@yucatan.gob.mx>
2021-05-13 17:47:52 -0500

    Favicon icons

--- public/resources/layouts/links-header-admin.php
+++ public/resources/layouts/links-header-admin.php
@@ -9,7 +9,7 @@
 	  <meta name="author" content="AleviRDZ">
 	  <meta name="keywords" content="site, web, sistem">
     <!-- Favicon -->
-    <link rel="shortcut icon" href="https://picsum.photos/30/30" />
+    <link rel="shortcut icon" href="../../../assets/theme_img/diseno-de-interfaz-de-usuario.svg" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <!-- Web page CSS -->
