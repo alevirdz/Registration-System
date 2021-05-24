62d4989e05a98598bf347e4fc4c5e18b8f0056e6
youalevi@gmail.com <youalevi@gmail.com>
2021-05-23 23:13:59 -0500

    update

--- public/dashboard/Maps.php
+++ public/dashboard/Maps.php
@@ -1,3 +1,16 @@
+<div class="row">
+<div class="col-md-12 col-lg-6">
+    <div class="card">
+        <div class="card-header">
+        <h5 class="card-title">Mapa ubicación del centro</h5>
+        <h6 class="card-subtitle text-muted">Se muestra el mapa de Parque central</h6>
+    </div>
+    <div class="card-body text-center">
+        <div class="row">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1862.8216705335974!2d-89.62448029093893!3d20.966833417247162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5671623c0fd4c3%3A0xda42c2632f3ddfe2!2sPlaza%20Principal%20de%20M%C3%A9rida%20%22Plaza%20Grande%22!5e0!3m2!1ses-419!2smx!4v1619940404307!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+</div></div>
 
 <div class="col-md-12 col-lg-6">
     <div class="card">
@@ -11,16 +24,4 @@
         </div>
     </div>
 </div>
-
-<div class="col-md-12 col-lg-6">
-    <div class="card">
-        <div class="card-header">
-        <h5 class="card-title">Mapa ubicación del centro</h5>
-        <h6 class="card-subtitle text-muted">Se muestra el mapa de Parque central</h6>
-    </div>
-    <div class="card-body text-center">
-        <div class="row">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1862.8216705335974!2d-89.62448029093893!3d20.966833417247162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5671623c0fd4c3%3A0xda42c2632f3ddfe2!2sPlaza%20Principal%20de%20M%C3%A9rida%20%22Plaza%20Grande%22!5e0!3m2!1ses-419!2smx!4v1619940404307!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-        </div>
-    </div>
 </div>
